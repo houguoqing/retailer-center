29bfe807ddbea532859ac361fa03b4e57a62caef
DE <7862422+hgq2430@user.noreply.gitee.com>
2021-05-16 13:21:23 +0800

    初始化Spring Boot工程

---
+++ src/main/java/com/kkb/retailercenter/RetailerCenterApplication.java
@@ -0,0 +1,13 @@
+package com.kkb.retailercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailerCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailerCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkb/retailercenter/RetailerCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkb.retailercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailerCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
